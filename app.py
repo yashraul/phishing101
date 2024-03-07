a6c00a1d5f82b6669503276f406262a7d9a4285d
yashraul <raulyash57@gmail.com>
2024-03-07 00:18:53 +0000

    made changes to make the classifier to stay on no additional features

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import pickle
+import re
 
 app = Flask(__name__)
 
-# Load the trained model
+# Load the trained model and CountVectorizer
 model = pickle.load(open("model.pkl", "rb"))
-
-# Load the CountVectorizer
 count_vect = pickle.load(open("count_vect.pkl", "rb"))
 
 
+# Define a function to preprocess the URL
+def preprocess_url(url):
+    # Preprocess the URL (similar to training data preprocessing)
+    url = re.sub(r"[^\w\s]", "", url)
+    return url
+
+
 @app.route("/")
-def Home():
+def home():
     return render_template("index.html")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    prediction = model.prdict()
-    return render_template(
-        "index.html", prediction_text="this website is".format(prediction)
-    )
+    if request.method == "POST":
+        url = request.form["url"]
+        # Preprocess the URL
+        url = preprocess_url(url)
+        # Vectorize the URL using the same CountVectorizer
+        url_vectorized = count_vect.transform([url])
+        # Predict phishing or legitimate
+        prediction = model.predict(url_vectorized)
+        if prediction[0] == 1:
+            result = "Phishing Website Detected"
+        else:
+            result = "Legitimate Website"
+        return render_template("index.html", prediction_text=result)
 
 
 if __name__ == "__main__":
